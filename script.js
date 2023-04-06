bbfb0d0643e62093970dd601d238a1eadab51cc1
DavidClegg <i.am.david.clegg@gmail.com>
2023-04-06 20:23:48 +0100

    update 2 - shuffled deck

--- script.js
+++ script.js
@@ -11,13 +11,13 @@ for(let suit = 0; suit < suits.length; suit++){
 console.log("CREATED DECK")
 console.log(deck)
 // Shuffle
-//deck = shuffle(deck);
+deck = shuffle(deck);
 
 function shuffle(deck){
   let newDeck = new Array(deck.length);
   newDeck.fill(0);
   for(let card of deck){
-    let spot = Math.floor(Math.random() * deck.length-1);
+    let spot = Math.floor(Math.random() * (deck.length-1));
     while(newDeck[spot]){ 
       // if the newDeck[spot] has a value, then generate a new spot
       // I would like to make this better, but computers are fast enough
@@ -31,18 +31,21 @@ console.log("SHUFFLED DECK")
 console.log(deck)
 
 
-
 // Deal Cards
 const stuckdeck = document.querySelector("#stuck-deck");
 deck.forEach(card => {console.log(card);stuckdeck.appendChild(card)})
 console.log("CARDS DEALT TO STUCK DECK")
 
+// Deal to other decks
+
 const cards = document.querySelectorAll(".card");
+
+
+
 const decks = document.querySelectorAll(".deck");
 let dragged = null;
 
 [...cards].forEach(card=>{
-  // card.innerHTML = randomCard();
   card.addEventListener("dragstart", dragstart);
 })
 
@@ -64,19 +67,21 @@ function drop(event){
 
   // drop on an empty space?
 
-
-  if(event.target.className == "deck"){
+  if(event.target.className == "deck" || event.target.className == "card"){
+    console.log(event);
     dragged.parentNode.removeChild(dragged);
     event.target.appendChild(dragged);
-    console.log(event);
   }
 }
 
 function Card(suit, value){
   let card = document.createElement("div");
+  let p = document.createElement("p");
+  p.classList.add("unselectable");
   let text = document.createTextNode(value + "" + suit);
   card.classList.add("card")
-  card.appendChild(text);
+  p.appendChild(text);
+  card.appendChild(p)
   card.dataset.suit = suit;
   card.dataset.value = value
   card.draggable = true;
